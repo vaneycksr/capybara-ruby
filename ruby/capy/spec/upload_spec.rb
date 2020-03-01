4561282b0345d84375b61133c8ec75a746ab8af8
vaneycksr <vaneyck23@gmail.com>
2020-02-29 23:48:45 -0300

    realizando upload de arquivos de texto e imagens

---
+++ ruby/capy/spec/upload_spec.rb
@@ -0,0 +1,44 @@
+describe "Upload", :upload do
+  before(:each) do
+    visit "https://training-wheels-protocol.herokuapp.com/upload"
+
+    # retorna o caminho relativo da pasta do projeto (serve para qualquer sistema operacional)
+    # a variavel com o @ eh uma variavel de instancia
+    @arquivo = Dir.pwd + "/spec/fixtures/arquivo.txt"
+
+    @imagem = Dir.pwd + "/spec/fixtures/ruby.png"
+  end
+
+  it "upload com arquivo de texto" do
+
+    # metodo usado pra anexar o arquivo
+    # entre parenteses -> (identificador do elemento (botao), arquivo)
+    attach_file("file-upload", @arquivo)
+
+    click_button "Upload"
+
+    div_arquivo = find("#uploaded-file")
+    expect(div_arquivo.text).to eql "arquivo.txt"
+  end
+
+  it "upload de imagem" do
+    attach_file("file-upload", @imagem)
+
+    click_button "Upload"
+
+    sleep 3
+
+    #buscar elemento da imagem
+    img = find("#new-image")
+
+    # como a variavel eh um elemento do capybara, posso acessar qualquer atributo desse elemento
+    expect(img[:src]).to include "/uploads/ruby.png"
+
+    #div_imagem = find("#uploaded-file")
+    #expect(div_imagem.text).to eql "ruby.png"
+  end
+
+  after(:each) do
+    sleep 3
+  end
+end
