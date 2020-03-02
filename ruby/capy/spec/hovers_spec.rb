a6674c7b27a3edce5d6b779cad5d6d341e979836
vaneycksr <vaneyck23@gmail.com>
2020-03-01 22:01:36 -0300

    aprendendo a utilizar o mouse hover

---
+++ ruby/capy/spec/hovers_spec.rb
@@ -0,0 +1,31 @@
+describe "Mouse Hover", :houvers do
+  before(:each) do
+    visit "https://training-wheels-protocol.herokuapp.com/hovers"
+  end
+
+  it "quando passo o mouse sobre o blade" do
+    card = find("img[alt=Blade]")
+
+    # metodo houver faz a função de passar o mouse pela imagem
+    card.hover
+
+    # verifica se ao passar o mouse na imagem, eh exibido uma frase
+    expect(page).to have_content "Nome: Blade"
+  end
+
+  it "quando passo o mouse sobre o pantera negra" do
+    card = find('img[alt="Pantera Negra"]')
+
+    card.hover
+
+    expect(page).to have_content "Nome: Pantera Negra"
+  end
+
+  it "quando passo a mouse sobre o homem aranha" do
+    card = find('img[alt="Homem Aranha"]')
+
+    card.hover
+
+    expect(page).to have_content "Nome: Homem Aranha"
+  end
+end
