67bbd5edf134be08207a507690313bc4d6f1e485
vaneycksr <vaneyck23@gmail.com>
2020-03-05 21:21:14 -0300

    criando um array de letras e verificando o resultado na pagina

--- ruby/capy/spec/key_spec.rb
+++ ruby/capy/spec/key_spec.rb
@@ -5,9 +5,27 @@ describe "Teclado", :key do
 
   it "enviando teclas" do
 
-    #simulando envio de teclas
-    find("#campo-id").send_keys :space
+    # array de simbolos
+    teclas = %i[tab escape space enter shift control alt]
 
-    expect(page).to have_content "You entered: SPACE"
+    # foreach no ruby
+    teclas.each do |t|
+
+      #simulando envio de teclas
+      find("#campo-id").send_keys t
+
+      # to_s converte para string
+      expect(page).to have_content "You entered: " + t.to_s.upcase
+    end
+  end
+
+  it "enviando letras" do
+    letras = %w[a s d f g j k l]
+
+    letras.each do |l|
+      find("#campo-id").send_keys l
+
+      expect(page).to have_content "You entered: " + l.upcase
+    end
   end
 end
